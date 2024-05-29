2fce6ff1ba3c71588fe5b14dee331cf476f39c4a
nguyendanhtruong <truong626215@gmail.com>
2024-05-29 17:48:14 +0700

    lesson6

---
+++ lesson6/Ndtlesson6/Ndtlesson6/Controllers/NdtSongController.cs
@@ -0,0 +1,40 @@
+﻿using Ndtlesson6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ndtlesson6.Controllers
+{
+    public class NdtSongController : Controller
+    {
+        private static List<NdtSong> ndtSongs = new List<NdtSong>()
+        {
+            new NdtSong {Id=1,NdtTitle="Nguyễn Danh Trường",NdtAuthor="K22CNTT4",NdtArlist="NTU",NdtYearRelease=2020},
+            new NdtSong {Id=2,NdtTitle="Nguyễn Danh T",NdtAuthor="K22CNTT4",NdtArlist="NTU",NdtYearRelease=2020},
+        };
+        // GET: NdtSong
+        /// <summary>
+        /// NdtAuthor:Nguyễn Danh Trường
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NdtIndex()
+        {
+            return View(ndtSongs);
+        }
+        // GET:NdtCreate
+        /// <summary>
+        /// Form Thêm mới bài hát 
+        /// NdtAuthor:Nguyễn Danh Trường
+
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult NdtCreate()
+        {
+            var ndtSong = new NdtSong();
+            return View(ndtSong);
+
+        }
+    }
+}
---
+++ lesson6/Ndtlesson6/Ndtlesson6/Models/NdtSong.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ndtlesson6.Models
+{
+    public class NdtSong
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="hãy nhập tên tiêu đề")]
+        [DisplayName("Tiêu đề")]
+        public string NdtTitle { get; set; }
+        [Required(ErrorMessage ="tên các giả ")]
+        [DisplayName("Tên Tác giả ")]
+
+
+        public string NdtAuthor { get; set; }
+        [Required(ErrorMessage ="tên nghệ sĩ ")]
+        [DisplayName("Nghệ sĩ")]
+
+        public string NdtArlist { get; set; }
+        [Required(ErrorMessage =" năm xuất bản")]
+        [RegularExpression(@"[0-9]{4}",ErrorMessage ="Ndt: nhập năm xuất bản 2 kí tự số ")]
+        [Range(1900,2024,ErrorMessage =" nhập giới hạn từ 1900-2024")]
+        [DisplayName("Năm")]
+
+        public int NdtYearRelease { get; set; }
+
+
+    }
+}
