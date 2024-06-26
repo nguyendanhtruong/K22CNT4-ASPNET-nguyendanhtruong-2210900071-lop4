5ded784ad671c4ad370bc0e38c363de88eb4ab65
nguyendanhtruong <truong626215@gmail.com>
2024-06-26 16:50:30 +0700

    ndtlesson09

---
+++ ndtlesson09/ndtlesson09/Controllers/NdtKhoaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ndtlesson09.Models;
+
+namespace ndtlesson09.Controllers
+{
+    public class NdtKhoaController : Controller
+    {
+        private qlsinhvienEntities db = new qlsinhvienEntities();
+
+        // GET: NdtKhoa
+        public ActionResult NdtIndex()
+        {
+            return View(db.khoas.ToList());
+        }
+
+        // GET: NdtKhoa/Details/5
+        public ActionResult NdtDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            khoa khoa = db.khoas.Find(id);
+            if (khoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoa);
+        }
+
+        // GET: NdtKhoa/Create
+        public ActionResult NdtCreate()
+        {
+            return View();
+        }
+
+        // POST: NdtKhoa/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NdtCreate([Bind(Include = "MaKhoa,TenKhoa")] khoa khoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.khoas.Add(khoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(khoa);
+        }
+
+        // GET: NdtKhoa/Edit/5
+        public ActionResult NdtEdit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            khoa khoa = db.khoas.Find(id);
+            if (khoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoa);
+        }
+
+        // POST: NdtKhoa/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NdtEdit([Bind(Include = "MaKhoa,TenKhoa")] khoa khoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("NdtIndex");
+            }
+            return View(khoa);
+        }
+
+        // GET: NdtKhoa/Delete/5
+        public ActionResult NdtDelete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            khoa khoa = db.khoas.Find(id);
+            if (khoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khoa);
+        }
+
+        // POST: NdtKhoa/Delete/5
+        [HttpPost, ActionName("NdtDelete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            khoa khoa = db.khoas.Find(id);
+            db.khoas.Remove(khoa);
+            db.SaveChanges();
+            return RedirectToAction("NdtIndex");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ndtlesson09/ndtlesson09/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ndtlesson09.MvcApplication" Language="C#" %>
